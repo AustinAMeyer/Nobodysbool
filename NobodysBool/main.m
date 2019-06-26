be62d7a865c87fe5868846d9d662e75666c4b80b
Austin <austin@tag219877.lrc.txstate.edu>
2019-06-26 12:39:34 -0500

    Initial Commit

---
+++ NobodysBool/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  NobodysBool
+//
+//  Created by Austin on 6/25/19.
+//  Copyright © 2019 Austin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    
+    //hasLicnese shows that the driver has a valid US drivers license
+    bool hasLicense;
+    
+    /*Block
+     of
+     comments
+     */
+    
+    hasLicense = false;
+    
+    NSLog(@"Has Licnese: %i", hasLicense);
+    
+    return 0;
+}
